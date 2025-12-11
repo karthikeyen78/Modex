d3ff1fc755ff3896d931ce0d143f4dd53caa73e4
karthikeyen78 <kkarthikeyen18@gmail.com>
2025-12-11 17:29:23 +0530

    Complete full stack implementation with frontend

--- Modex task/db/index.js
+++ Modex task/db/index.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.SUPABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // Fix for local postgres: usually doesn't support SSL by default
+  ssl: process.env.SUPABASE_URL.includes("localhost")
+    ? false
+    : { rejectUnauthorized: false },
 });
 
 module.exports = pool;
